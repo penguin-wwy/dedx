365d510565f2147d99df71e230580ac2a9ccb37a
penguin-wwy <940375606@qq.com>
2019-08-08 01:16:17 +0800

    add visit trycatch block

--- src/main/kotlin/com/dedx/transform/MethodTransformer.kt
+++ src/main/kotlin/com/dedx/transform/MethodTransformer.kt
@@ -8,6 +8,8 @@ import com.dedx.dex.struct.DexNode
 import com.dedx.dex.struct.InstNode
 import com.dedx.dex.struct.MethodInfo
 import com.dedx.dex.struct.MethodNode
+import com.dedx.tools.Configuration
+import com.dedx.utils.DecodeException
 import org.objectweb.asm.Label
 import java.lang.Exception
 import kotlin.collections.ArrayList
@@ -43,21 +45,59 @@ class MethodTransformer(val mthNode: MethodNode, val clsTransformer: ClassTransf
         if (mthNode.debugInfoOffset != DexNode.NO_INDEX) {
             MethodDebugInfoVisitor.visitMethod(mthNode)
         }
-        CFGBuildPass.visit(this)
-        dfInfo = DataFlowAnalysisPass.visit(this)
-        DataFlowAnalysisPass.livenessAnalyzer(dfInfo!!)
+        if (Configuration.optimization) {
+            visitOptimization()
+        } else {
+            visitNormal()
+        }
+    }
+
+    private fun visitNormal() {
         try {
             mthVisit.visitCode()
-            val label0 = Label()
-            currBlock = blockMap.getOrPut(label0) {
-                visitLabel(label0)
-                return@getOrPut BasicBlock.create(label0, null)
-            }
+            visitTryCatchBlock()
+            var prevLineNumber = 0
             for (inst in mthNode.codeList) {
-
+                if (inst != null) process(inst, prevLineNumber)
             }
         } catch (e: Exception) {
             e.printStackTrace()
+        }
+    }
+
+    private fun visitOptimization() {
+        CFGBuildPass.visit(this)
+        dfInfo = DataFlowAnalysisPass.visit(this)
+        DataFlowAnalysisPass.livenessAnalyzer(dfInfo!!)
+        // TODO tranform by optimize
+    }
+
+    private fun visitTryCatchBlock() {
+        for (tcBlock in mthNode.tryBlockList) {
+            val start = tcBlock.instList[0]
+            val end = tcBlock.instList.last()
+            if (start.getLabel() == null) {
+                start.setLable(Label())
+            }
+            if (end.getLabel() == null) {
+                end.setLable(Label())
+            }
+            for (exec in tcBlock.execHandlers) {
+                val catchInst = code(exec.addr)
+                if (catchInst == null) {
+                    // TODO
+                } else {
+                    if (catchInst.getLabel() == null) {
+                        catchInst.setLable(Label())
+                    }
+                    val startLabel = start.getLabel()?.value ?: throw DecodeException("TryCatch block empty")
+                    val endLabel = end.getLabel()?.value ?: throw DecodeException("TryCatch block empty")
+                    val catchLabel = catchInst.getLabel()?.value ?: throw DecodeException("TryCatch block empty")
+                    for (type in exec.catchTypes) {
+                        mthVisit.visitTryCatchBlock(startLabel, endLabel, catchLabel, type?.className())
+                    }
+                }
+            }
         }
     }
 
@@ -83,7 +123,14 @@ class MethodTransformer(val mthNode: MethodNode, val clsTransformer: ClassTransf
         }
     }
 
-    private fun process(inst: InstNode) {
+    private fun process(inst: InstNode, prevLineNumber: Int) {
+        if (inst.getLabel() != null) {
+            if (inst.getLineNumber() != prevLineNumber) {
+                visitLabel(inst.getLabel()?.value, inst.getLineNumber())
+            } else {
+                visitLabel(inst.getLabel()?.value, null)
+            }
+        }
         when (inst.instruction.opcode) {
             Opcodes.INVOKE_DIRECT -> {
                 visitInvokeDirect(inst, InvokeType.INVOKESPECIAL)
@@ -97,8 +144,9 @@ class MethodTransformer(val mthNode: MethodNode, val clsTransformer: ClassTransf
         mthVisit.visitMethodInsn(invokeType, mthInfo.declClass.className(), mthInfo.name, mthInfo.parseSignature(), false)
     }
 
-    private fun visitLabel(label0: Label) {
+    private fun visitLabel(label0: Label?, lineNumber: Int?) {
+        if (label0 == null) return
         mthVisit.visitLabel(label0)
-//        mthVisit.visitLineNumber()
+        if (lineNumber != null) mthVisit.visitLineNumber(lineNumber, label0)
     }
 }
