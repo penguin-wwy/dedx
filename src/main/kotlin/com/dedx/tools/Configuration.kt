0e560808581e4d07a033f77dc665d90830212ac2
penguin-wwy <940375606@qq.com>
2019-09-23 23:51:16 +0800

    add command

--- src/main/kotlin/com/dedx/tools/Configuration.kt
+++ src/main/kotlin/com/dedx/tools/Configuration.kt
@@ -5,4 +5,6 @@ object Configuration {
     const val NormalOpt = 1
     const val Optimized = 2
     var optLevel = 0
+    lateinit var outDir: String
+    lateinit var dexFiles: MutableList<String>
 }
--- src/main/kotlin/com/dedx/tools/Main.kt
+++ src/main/kotlin/com/dedx/tools/Main.kt
@@ -1,2 +1,47 @@
 package com.dedx.tools
 
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+
+fun createCmdTable(): Options {
+    val help = Option.builder("h")
+            .longOpt("help").desc("Print help message").build()
+    val version = Option.builder("v")
+            .longOpt("version").desc("Print version").build()
+    val output = Option.builder("o")
+            .longOpt("output")
+            .hasArg().argName("dirname").desc("Specify output dirname").build()
+
+    val optTable = Options()
+    optTable.addOption(help)
+    optTable.addOption(version)
+    optTable.addOption(output)
+
+    return optTable
+}
+
+fun configFromOptions(args: Array<String>, optTable: Options) {
+    val parser = DefaultParser()
+    try {
+        val cmdTable = parser.parse(optTable, args)
+        if (cmdTable.hasOption("v") || cmdTable.hasOption("version")) {
+            System.exit(0)
+        }
+        if (cmdTable.hasOption("h") || cmdTable.hasOption("help")) {
+            HelpFormatter().printHelp("command [options] <dexfile>", optTable)
+            System.exit(0)
+        }
+        if (cmdTable.hasOption("o") || cmdTable.hasOption("output")) {
+            Configuration.outDir = cmdTable.getOptionValue("o") ?: cmdTable.getOptionValue("output")
+        }
+        Configuration.dexFiles = cmdTable.argList
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+fun main(args: Array<String>) {
+    configFromOptions(args, createCmdTable())
+}
