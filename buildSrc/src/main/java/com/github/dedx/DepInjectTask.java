c7030fb7536de4279c6a0cb918eb38096c03b591
penguin-wwy <940375606@qq.com>
2019-10-19 22:20:54 +0800

    add class file collect in Java depInject

--- buildSrc/src/main/java/com/github/dedx/DepInjectTask.java
+++ buildSrc/src/main/java/com/github/dedx/DepInjectTask.java
@@ -1,22 +1,34 @@
 package com.github.dedx;
 
+import com.google.common.flogger.FluentLogger;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.TaskAction;
 
-import java.io.File;
-import java.util.Set;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public abstract class DepInjectTask extends DefaultTask {
 
+    static {
+        Logger root = Logger.getLogger("");
+        root.addHandler(new ConsoleHandler());
+        root.setLevel(Level.INFO);
+    }
+
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
     public boolean enable = false;
     public FileCollection classFileCollection = null;
 
     @TaskAction
     public void depInjectAction() {
-        System.out.println(String.format("dep inject action [%s]...", getLanguageName()));
-        runAction();
-        System.out.println("dep inject action finish");
+        logger.atInfo().log("dep inject action [%s]...", getLanguageName());
+        if (enable) {
+            runAction();
+        }
+        logger.atInfo().log("dep inject action finish");
     }
 
     protected abstract void runAction();
--- buildSrc/src/main/java/com/github/dedx/JavaDepInjectTask.java
+++ buildSrc/src/main/java/com/github/dedx/JavaDepInjectTask.java
@@ -1,16 +1,34 @@
 package com.github.dedx;
 
+import com.google.common.collect.Sets;
+import com.google.common.flogger.FluentLogger;
+
+import java.io.File;
+import java.util.*;
+
 public class JavaDepInjectTask extends DepInjectTask {
 
     private final String languageName = "java";
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
     @Override
     protected void runAction() {
-
+        Set<File> classFiles = Sets.newHashSet();
+        classFileCollection.getFiles().forEach((buildDir) -> {
+            if (buildDir.getAbsolutePath().endsWith(languageName + File.separator + "main")) {
+                File[] files = buildDir.listFiles((File file, String s) -> s.endsWith(".class"));
+                if (files != null) classFiles.addAll(Arrays.asList(files));
+            }
+        });
+        injectFiles(classFiles);
     }
 
     @Override
     public String getLanguageName() {
         return languageName;
+    }
+
+    private void injectFiles(Collection<File> classFiles) {
+
     }
 }
