d4f191a3b50bcf0ce00e61963e140347baa29ae8
penguin-wwy <940375606@qq.com>
2019-10-21 23:45:10 +0800

    add log and refomt in ClassTransformer

--- src/main/kotlin/com/dedx/transform/ClassTransformer.kt
+++ src/main/kotlin/com/dedx/transform/ClassTransformer.kt
@@ -4,6 +4,7 @@ import com.android.dx.rop.code.AccessFlags
 import com.dedx.dex.struct.ClassInfo
 import com.dedx.dex.struct.ClassNode
 import com.dedx.dex.struct.MethodNode
+import com.google.common.flogger.FluentLogger
 import org.objectweb.asm.ClassWriter
 import org.objectweb.asm.FieldVisitor
 import org.objectweb.asm.Opcodes.V1_8
@@ -14,24 +15,30 @@ class ClassTransformer(val clsNode: ClassNode, val filePath: String = "") {
     val classWriter = ClassWriter(0)
     lateinit var fieldVisitor: FieldVisitor
 
+    companion object {
+        private val logger = FluentLogger.forEnclosingClass()
+    }
+
     fun visitClass(): ClassTransformer {
         classWriter.visit(V1_8,
                 if (!clsNode.isInterface()) clsNode.accFlags or AccessFlags.ACC_SUPER else clsNode.accFlags,
                 clsNode.clsInfo.fullName.replace('.', '/'),
                 null,
                 if (clsNode.hasSuperClass()) clsNode.superClassNameWithSlash() else ClassInfo.ROOT_CLASS_NAME,
-                if (clsNode.interfaces.isNotEmpty()) Array(clsNode.interfaces.size) {
-                    i -> clsNode.interfaces[i].nameWithSlash()
+                if (clsNode.interfaces.isNotEmpty()) Array(clsNode.interfaces.size) { i ->
+                    clsNode.interfaces[i].nameWithSlash()
                 } else null)
 
         // TODO: set source file need debug info
 //        classWriter.visitSource()
 
-        clsNode.fields.forEach { field -> run {
-            fieldVisitor = classWriter.visitField(field.accFlags, field.fieldInfo.name, field.fieldInfo.type.descriptor(), null, null)
-            // TODO: fieldVisitor.visitAnnotation()
-            fieldVisitor.visitEnd()
-        } }
+        clsNode.fields.forEach { field ->
+            run {
+                fieldVisitor = classWriter.visitField(field.accFlags, field.fieldInfo.name, field.fieldInfo.type.descriptor(), null, null)
+                // TODO: fieldVisitor.visitAnnotation()
+                fieldVisitor.visitEnd()
+            }
+        }
 
         var main: MethodNode? = null
         for (mthNode in clsNode.methods) {
@@ -47,10 +54,11 @@ class ClassTransformer(val clsNode: ClassNode, val filePath: String = "") {
     }
 
     fun dump(): String {
-            FileOutputStream(File(filePath)).use {
-                w -> w.write(classWriter.toByteArray())
-            }
-            return filePath
+        FileOutputStream(File(filePath)).use { w ->
+            w.write(classWriter.toByteArray())
+        }
+        logger.atInfo().log("Dump ${clsNode.clsInfo.fullName} to $filePath")
+        return filePath
     }
 
     fun toFile(path: String) = FileOutputStream(File(path)).write(classWriter.toByteArray())
