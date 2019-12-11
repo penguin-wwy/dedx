5fa04bc3bc56afdc23e8a9fd172c8bba8aa8e2b8
penguin-wwy <940375606@qq.com>
2019-12-11 23:28:59 +0800

    add jvmInst toString

--- src/main/kotlin/com/dedx/transform/JvmInst.kt
+++ src/main/kotlin/com/dedx/transform/JvmInst.kt
@@ -3,6 +3,9 @@ package com.dedx.transform
 import com.android.dx.io.instructions.FillArrayDataPayloadDecodedInstruction
 import com.android.dx.io.instructions.PackedSwitchPayloadDecodedInstruction
 import com.android.dx.io.instructions.SparseSwitchPayloadDecodedInstruction
+import com.dedx.dex.struct.DexNode
+import com.dedx.dex.struct.FieldInfo
+import com.dedx.dex.struct.MethodInfo
 import com.dedx.utils.DecodeException
 import org.objectweb.asm.Label
 import org.objectweb.asm.MethodVisitor
@@ -33,6 +36,8 @@ class LabelInst() {
         }
         return value as Label
     }
+
+    override fun toString() = value?.toString() ?: ""
 }
 
 interface JvmInst: Opcodes {
@@ -126,6 +131,17 @@ abstract class JvmInst2(override var label: LabelInst): JvmInst {
     init {
         label.inst = this
     }
+
+    companion object {
+        fun formatString(label: LabelInst, ops: Int, vararg values: Any): String {
+            val result = StringBuilder(if (label.getValue() != null) "$label:\n" else "")
+            result.append("\t${Ops.toString(ops)}  ")
+            values.forEach { result.append("$it  ") }
+            return result.toString()
+        }
+    }
+
+    abstract fun toString(dex: DexNode): String
 }
 
 class SingleInst(override val opcodes: Int, label: LabelInst): JvmInst2(label) {
@@ -134,6 +150,12 @@ class SingleInst(override val opcodes: Int, label: LabelInst): JvmInst2(label) {
     override fun visitInst(transformer: InstTransformer) {
         transformer.methodVisitor().visitInsn(opcodes)
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes)
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 class SlotInst(override val opcodes: Int, label: LabelInst, val slot: Int): JvmInst2(label) {
@@ -159,6 +181,12 @@ class SlotInst(override val opcodes: Int, label: LabelInst, val slot: Int): JvmI
     fun isLoadInst() = if (opcodes in Opcodes.ILOAD..Opcodes.ALOAD) true else false
 
     fun isStoreInst() = if (opcodes in Opcodes.ISTORE..Opcodes.ASTORE) true else false
+
+    override fun toString(): String {
+        return formatString(label, opcodes, slot)
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 class IntInst(override val opcodes: Int, label: LabelInst, val number: Int): JvmInst2(label) {
@@ -166,6 +194,12 @@ class IntInst(override val opcodes: Int, label: LabelInst, val number: Int): Jvm
     override fun visitInst(transformer: InstTransformer) {
         transformer.methodVisitor().visitIntInsn(opcodes, number)
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, number)
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 class LiteralInst(override val opcodes: Int, label: LabelInst, val literal: Long, val type: SlotType): JvmInst2(label) {
@@ -183,6 +217,20 @@ class LiteralInst(override val opcodes: Int, label: LabelInst, val literal: Long
             SlotType.DOUBLE -> mthVisitor.visitLdcInsn(Double.fromBits(literal))
         }
     }
+
+    private fun toNumber() = when (type) {
+        SlotType.INT -> literal.toInt()
+        SlotType.LONG -> literal
+        SlotType.FLOAT -> Float.fromBits(literal.toInt())
+        SlotType.DOUBLE -> Double.fromBits(literal)
+        else -> throw DecodeException("Format string to number error.")
+    }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, toNumber())
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 class TypeInst(override val opcodes: Int, label: LabelInst, val typeString: String): JvmInst2(label) {
@@ -194,6 +242,12 @@ class TypeInst(override val opcodes: Int, label: LabelInst, val typeString: Stri
             else -> mthVisitor.visitTypeInsn(opcodes, typeString)
         }
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, typeString)
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 class ConstantInst(override val opcodes: Int, label: LabelInst, val constIndex: Int): JvmInst2(label) {
@@ -204,6 +258,12 @@ class ConstantInst(override val opcodes: Int, label: LabelInst, val constIndex: 
             Opcodes.LDC -> mthVisitor.visitLdcInsn(transformer.string(constIndex))
         }
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, "#$constIndex")
+    }
+
+    override fun toString(dex: DexNode) = formatString(label, opcodes, dex.getString(constIndex))
 }
 
 class InvokeInst(override val opcodes: Int, label: LabelInst, val invokeType: Int, val mthIndex: Int): JvmInst2(label) {
@@ -217,6 +277,12 @@ class InvokeInst(override val opcodes: Int, label: LabelInst, val invokeType: In
         transformer.methodVisitor()
                 .visitMethodInsn(invokeType, mthInfo.declClass.className(), mthInfo.name, mthInfo.parseSignature(), isInterface)
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, "#$mthIndex")
+    }
+
+    override fun toString(dex: DexNode) = formatString(label, opcodes, MethodInfo.fromDex(dex, mthIndex))
 }
 
 class JumpInst(override val opcodes: Int, label: LabelInst, var target: LabelInst): JvmInst2(label) {
@@ -224,6 +290,12 @@ class JumpInst(override val opcodes: Int, label: LabelInst, var target: LabelIns
     override fun visitInst(transformer: InstTransformer) {
         transformer.methodVisitor().visitJumpInsn(opcodes, target.getValueOrCreate())
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, target)
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 class FieldInst(override val opcodes: Int, label: LabelInst, val fieldIndex: Int): JvmInst2(label) {
@@ -233,6 +305,12 @@ class FieldInst(override val opcodes: Int, label: LabelInst, val fieldIndex: Int
         transformer.methodVisitor()
                 .visitFieldInsn(opcodes, fieldInfo.declClass.className(), fieldInfo.name, fieldInfo.type.descriptor())
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, "#$fieldIndex")
+    }
+
+    override fun toString(dex: DexNode) = formatString(label, opcodes, FieldInfo.fromDex(dex, fieldIndex))
 }
 
 class FillArrayDataPayloadInst(override val opcodes: Int, label: LabelInst, val slot: Int,
@@ -306,6 +384,12 @@ class FillArrayDataPayloadInst(override val opcodes: Int, label: LabelInst, val 
             }
         }
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, slot, target, type)
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 /*
@@ -324,6 +408,12 @@ class PackedSwitchPayloadInst(override val opcodes: Int, label: LabelInst,
         }
         mthVisitor.visitTableSwitchInsn(payload.firstKey, payload.firstKey + labelArray.size - 1, defaultLabel.getValue() ?: return, *labelArray)
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, target, defaultLabel)
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 class SparseSwitchPayloadInst(override val opcodes: Int, label: LabelInst,
@@ -340,6 +430,12 @@ class SparseSwitchPayloadInst(override val opcodes: Int, label: LabelInst,
         }
         mthVisitor.visitLookupSwitchInsn(defaultLabel.getValue() ?: return, caseArray, labelArray)
     }
+
+    override fun toString(): String {
+        return formatString(label, opcodes, target, defaultLabel)
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 class MultiANewArrayInsn(label: LabelInst, var typeName: String, val numDimensions: Int): JvmInst2(label) {
@@ -348,6 +444,12 @@ class MultiANewArrayInsn(label: LabelInst, var typeName: String, val numDimensio
     override fun visitInst(transformer: InstTransformer) {
         transformer.methodVisitor().visitMultiANewArrayInsn(typeName, numDimensions)
     }
+
+    override fun toString(): String {
+        return formatString(label, Opcodes.MULTIANEWARRAY, typeName, numDimensions)
+    }
+
+    override fun toString(dex: DexNode) = toString()
 }
 
 class ShadowInst(override val opcodes: Int, label: LabelInst, val literal: Long?, val regs: IntArray): JvmInst2(label) {
@@ -396,5 +498,11 @@ class ShadowInst(override val opcodes: Int, label: LabelInst, val literal: Long?
         }
         else -> throw DecodeException("ShadowInst convert error")
     }
+
+    override fun toString(): String {
+        return ""
+    }
+
+    override fun toString(dex: DexNode) = toString()
 
 }
